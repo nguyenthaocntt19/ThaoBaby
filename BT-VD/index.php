21a18b841c26075e612a26711117ff74856cbcc3
NguyenThao <nguyenthaocntt19@gmail.com>
2022-10-05 15:37:15 +0700

    first commit

---
+++ BT-VD/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "This  is a test";
+// This is a one-line c++ style comment
+/* This is a multi line comment
+yet another line of comment */
+echo("This is yet another test");
+print "Hello World";
+print("Hello World");
+
+?>
+</body>
+</html>
